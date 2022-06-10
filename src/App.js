b7f51e80ea9e730638374d12be083b861b41f1e1
Charmighodasara <charmighodasara9@gmail.com>
2022-06-10 15:51:49 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import { Route, Router, Switch } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import Departments from "./container/Departments/Departments";
 import Home from "./container/home/Home";
 
 function App() {
   return (
     <div >
       <Header />
-      <Home />
+      <Switch>
+        <Route path={'/'} exact component={Home}></Route>
+        <Route path={'/departments'} exact component={Departments}></Route>
+        
+      </Switch>
       <Footer />
     </div>
   );
