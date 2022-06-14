03a8175965fb30b9912c6fe83e39b73c297e926b
Charmighodasara <charmighodasara9@gmail.com>
2022-06-14 15:31:05 +0530

    changes1

--- src/container/login/signup/Login_signup.js
+++ src/container/login/signup/Login_signup.js
@@ -1,58 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login_signup(props) {
+    const [user, setUser] = useState('login')
     return (
         <main id="main">
-        <section id="appointment" className="appointment">
-          <div className="container">
-            <div className="section-title">
-              <h2>Login</h2>
-            </div>
-            <form action method="post" role="form" className="php-email-form">
-              <div className="row">
-                <div className="col-md-4 form-group">
-                  <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                  <div className="validate" />
+            <section id="appointment" className="appointment">
+                <div className="container">
+                    <div className="section-title">
+                        {
+                            user === 'login' ?
+                                <h2>Login</h2>
+                                :
+                                <h2>signup</h2>
+                        }
+                    </div>
+                    <div className="php-email-form">
+                        <div>{
+                            user === 'login' ?
+                                null
+                                :
+                                <div className="col-md-4 form-group mt-3 mt-md-0 " >
+                                    <input type="text" className="form-control" name="name" id="name" placeholder="Your name" />
+                                    <div className="validate" />
+                                </div>
+                        }
+                        </div>
+                        <div>
+                            <div className="col-md-4 form-group mt-3 mt-md-0">
+                                <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" />
+                                <div className="validate" />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-4 form-group">
+                                <input type="password" name="password" className="form-control" id="password" placeholder="Your password" />
+                                <div className="validate" />
+                            </div>
+                        </div>
+                        {
+                            user === 'login' ?
+                                <div className="text-center"><button type="submit">Login</button></div>
+                                :
+                                <div className="text-center"><button type="submit">signup</button></div>
+                        }
+                        <div className="row">
+                            <div className="col-md-4 form-group">
+                                {
+                                    user === 'login' ?
+                                        <div className="text-center">create an new account <button onClick={()=>setUser('signup')}>signup</button>
+                                        </div>
+                                        :
+                                        <div className="text-center">already an account <button onClick={()=>setUser('login')}>login</button>
+                                        </div>
+                                }
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-md-4 form-group mt-3 mt-md-0">
-                  <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                  <div className="validate" />
-                </div>
-                <div className="col-md-4 form-group mt-3 mt-md-0">
-                  <input type="tel" className="form-control" name="phone" id="phone" placeholder="Your Phone" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                  <div className="validate" />
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-md-4 form-group mt-3">
-                  <input type="datetime" name="date" className="form-control datepicker" id="date" placeholder="Appointment Date" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                  <div className="validate" />
-                </div>
-                <div className="col-md-4 form-group mt-3">
-                  <select name="department" id="department" className="form-select">
-                    <option value>Select Department</option>
-                    <option value="Department 1">Department 1</option>
-                    <option value="Department 2">Department 2</option>
-                    <option value="Department 3">Department 3</option>
-                  </select>
-                  <div className="validate" />
-                </div>
-              </div>
-              <div className="form-group mt-3">
-                <textarea className="form-control" name="message" rows={5} placeholder="Message (Optional)" defaultValue={""} />
-                <div className="validate" />
-              </div>
-              <div className="mb-3">
-                <div className="loading">Loading</div>
-                <div className="error-message" />
-                <div className="sent-message">Your appointment request has been sent successfully. Thank you!</div>
-              </div>
-              <div className="text-center"><button type="submit">Login</button></div>
-            </form>
-          </div>
-        </section>
-      </main>
-      
+            </section>
+        </main>
+
     );
 }
 
