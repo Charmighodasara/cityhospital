c5338d04ebf929bf220121f05352d2e90caede13
Charmighodasara <charmighodasara9@gmail.com>
2022-06-16 18:41:29 +0530

    changes

--- src/container/Reference/Reference.js
+++ src/container/Reference/Reference.js
@@ -2,23 +2,37 @@ import React, { useRef } from 'react';
 
 function Reference(props) {
     const nameRef = useRef();
-    const emailRef = useRef ();
+    const emailRef = useRef();
 
     const handlesubmit = () => {
-        console.log(nameRef.current.value , emailRef.current.value);
+        console.log(nameRef.current.value, emailRef.current.value);
 
-        // nameRef.current.style.border = '1px solid red';
+        nameRef.current.style.border = '1px solid green';
         nameRef.current.style.backgroundColor = 'pink';
         emailRef.current.focus();
     }
 
-   
-
     return (
         <div>
-            <input ref={nameRef} type='text' name='name' placeholder='name'/>
-            <input ref={emailRef} type='email' name='email' placeholder='email'/>
-            <button onClick={()=> handlesubmit()}>submit</button>
+            <section id="appointment" className="appointment">
+                <div className="php-email-form">
+                    <div>
+                        <div className="col-md-4 form-group mt-3 mt-md-0 " >
+                            <input ref={nameRef} type="text" className="form-control" name="name" id="name" placeholder="Your name" />
+                            <div className="validate" />
+                        </div>
+                    </div>
+                    <div>
+                        <div className="col-md-4 form-group mt-3 mt-md-0">
+                            <input ref={emailRef} type="email" className="form-control" name="email" id="email" placeholder="Your Email" />
+                            <div className="validate" />
+                        </div>
+                    </div>
+                    <div className="text-center">
+                        <button type="submit" onClick={() => handlesubmit()}>submit</button>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 }
