a5e55ed72def84c04fbad860a116ffe9a6cd61a9
Charmighodasara <charmighodasara9@gmail.com>
2022-08-25 18:50:05 +0530

    changes

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -22,9 +22,9 @@ function Alert(props) {
                     }
                 })
         }
+        setTimeout(() => {dispatch(resetAlert())}, 2000);
     }, [alert.text])
 
-    setTimeout(() => {dispatch(resetAlert())}, 2000);
 
     return (
         <div>
